56a71986f4b3684309828166c393edf8df100183
HarshPatel8279 <pharsh.spt@gmail.com>
2021-01-04 11:12:53 +0530

    User SignUp Successfully error solve using add WebSecurityConfig and add Getters and Setters in UserResponse class

--- src/main/java/com/SpringJwtTurf/models/response/UserResponse.java
+++ src/main/java/com/SpringJwtTurf/models/response/UserResponse.java
@@ -1,9 +1,13 @@
 package com.SpringJwtTurf.models.response;
 
 import com.SpringJwtTurf.documents.User;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
+@Document
+@Data
 public class UserResponse {
     private String name;
     private String phoneNumber;
